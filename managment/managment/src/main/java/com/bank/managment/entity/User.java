e1b30671ac0725aae6d777535f7d8a9ab5cfde31
jessicasalgadolgtm <jessica.salgado@sofka.com.co>
2025-09-18 17:29:20 -0500

    Add accounts relationship to User entity

--- managment/managment/src/main/java/com/bank/managment/entity/User.java
+++ managment/managment/src/main/java/com/bank/managment/entity/User.java
@@ -1,19 +1,31 @@
 package com.bank.managment.entity;
 
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import jakarta.persistence.Entity;
 import lombok.Setter;
 
+import java.util.List;
+
+
 @AllArgsConstructor
 @Getter
 @Setter
 @NoArgsConstructor
 @Entity
+
+
 public class User {
     private Long idUser;
     private String name;
     private String email;
     private String password;
+
+    // Un usuario puede tener varias cuentas
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Account> accounts;
 }
